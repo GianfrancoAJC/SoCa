705d8ea7a9177aed4ea972dd98dc9234470b608f
GianfrancoAJC <131214466+GianfrancoAJC@users.noreply.github.com>
2024-05-29 11:44:01 -0500

    Fixing SoCa 1.2.1 Primer prototipo

--- backend/app.py
+++ backend/app.py
@@ -1,45 +1,130 @@
-import requests, json
-from flask import Flask, request
+# Librearias empleadas
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+from flask import (
+    Flask, 
+    render_template, 
+    request,
+    jsonify
+)
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+import os
+from datetime import datetime
+import sys
 
+# Configuration
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/maintenancelocalsocadb'
+app.config['UPLOAD_FOLDER'] = ''
+db = SQLAlchemy(app)
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-FACEBOOK_APP_ID = '7477882075624318'
-SECRET_APP_KEY = 'ab6d3dc4207581df83902c06ba1cb5cc'
-PAGE_ACCESS_TOKEN = 'Working on it...'
+# Models
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
+    name = db.Column(db.String(100), nullable=True)
+    lastname = db.Column(db.String(100), nullable=True)
+    user_name = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.Integer(), nullable=True)
+
+    def __init__(self, name, lastname, user_name, password, email, phone_number):
+        self.name = name
+        self.lastname = lastname
+        self.user_name = user_name
+        self.password = password
+        self.email = email
+        self.phone_number = phone_number
+
+    def __repr__(self):
+        return '<User %r>' % (self.user_name)
+    
+class traindata(db.Model):
+    __tablename__ = 'traindata'
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
+    v1 = db.Column(db.Integer(), nullable=False)
+    v2 = db.Column(db.Integer(), nullable=False)
+    v3 = db.Column(db.Integer(), nullable=False)
+    result = db.Column(db.String(100), nullable=False)
+    modifited_at = db.Column(db.DateTime, default=datetime.now)
+    
+    def __init__(self, v1, v2, v3, result):
+        self.v1 = v1
+        self.v2 = v2
+        self.v3 = v3
+        self.result = result
+
+    def __repr__(self):
+        return '<Register %r>' % (self.id)
+
+# End Models --------------------------------------------------
+    
+with app.app_context():
+    db.create_all()
+
+# Endpoints
 
 @app.route('/', methods=['GET'])
-def handle_verification():
-    if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.challenge'):
-        if request.args.get('hub.verify_token') == '200':
-            return request.args.get('hub.challenge')
+def index():
+    return render_template('index.html')
+
+@app.route('/calculadora', methods=['GET'])
+def calculadora():
+    return render_template('calculadora.html')
+
+@app.route('/calcular', methods=['POST'])
+def calcular():
+    try:
+        v1 = int(request.form['v1'])
+        v2 = int(request.form['v2'])
+        v3 = int(request.form['v3'])
+        recomendaciones = []
+        if v1 > 120:
+            if (v1/v3 < v2*2 and v2 > 0) or (v1/v3 > 2 and v2 == 0):
+                recomendaciones.append('Existen posibilidades de que usted padezca de FOMO')
         else:
-            return 'Verification token mismatch', 403
-    else:
-        return 'Invalid request', 400
+            recomendaciones.append('No se detectaron problemas')
+        return render_template('resultados.html', recomendaciones = recomendaciones)
+    except Exception as e:
+        print(e)
+        print(sys.exc_info())
+        return jsonify({'success': False, 'message': 'Error in the calculation'}), 500
+    finally:
+        db.session.close()
 
-@app.route('/', methods=['POST'])
-def handle_messages():
-    payload = request.get_json()
-    for entry in payload['entry']:
-        for messaging_event in entry['messaging']:
-            if messaging_event.get('message'):
-                sender_id = messaging_event['sender']['id']
-                recipient_id = messaging_event['recipient']['id']
-                message_text = messaging_event['message']['text']
-                send_message(sender_id, message_text)
-    return 'ok'
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify({
+        'success': False,
+        'message': 'Method not allowed'
+    }), 405
 
-def send_message(sender_id, message_text):
-    url = f'https://graph.facebook.com/{sender_id}/messages'
-    data = {
-        'recipient': {'id': sender_id},
-        'message': {'text': message_text},
-    }
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {PAGE_ACCESS_TOKEN}',
-    }
-    requests.post(url, data=json.dumps(data), headers=headers)
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'message': 'Resource not found'
+    }), 404
 
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({
+        'success': False,
+        'message': 'Internal Server error'
+    }), 500
+
+# Run the app
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+else:
+    print('Importing {}'.format(__name__))
+
+# End of file
---
+++ backend/readme.md
@@ -0,0 +1,41 @@
+# Structure Project
+
+- Mantenimiento
+  - app/ + **init**.py
+  - models.py
+  - service.py
+    - confi/
+      - local.py
+      - qa.py
+      - production.py
+  - tests/
+    - test_controller.py
+    - test_service.py
+
+## Operations
+
+### Create Employee
+
+```
+curl -F "firstname=Juan" -F "lastname=perez" -F "age=20" -F "selectDepartment=eef11f69-ffe9-4078-ad09-16e31fe7f77c" -F "image=@cristiano.jpeg;type=image/jpeg" -X POST http://localhost:5002/employees
+
+{
+  "id": "27b79c53-edeb-4caf-9bcb-3725669eaa9d",
+  "message": "Employee Created successfully!",
+  "success": true
+}
+
+curl -X POST http://localhost:5002/employees
+{
+  "errors": [
+    "firstname is required",
+    "lastname is required",
+    "age is required",
+    "selectDepartment is required",
+    "image is required"
+  ],
+  "message": "Error creating employee",
+  "success": false
+}
+
+```
